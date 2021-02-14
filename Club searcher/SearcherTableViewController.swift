9e9b6037dbc221c44fc6188ec55469008523ec8f
JiHoonAHN <ahnjh1028@naver.com>
2021-02-14 19:45:10 +0900

    study

--- Club searcher/SearcherTableViewController.swift
+++ Club searcher/SearcherTableViewController.swift
@@ -7,29 +7,33 @@
 
 import UIKit
 
-class SearcherTableViewController: UITableViewController,UISearchBarDelegate {
-    
+class MyTableViewController: UITableViewController , UISearchBarDelegate{
+
     let data = ["Apples","Oranges","Pears","Bananas","Plums"]
     var filteredData:[String]!
+    
     @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         searchBar.delegate = self
         
         filteredData = data
-        
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
         return filteredData.count
     }
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
@@ -53,6 +57,8 @@ class SearcherTableViewController: UITableViewController,UISearchBarDelegate {
         }
         self.tableView.reloadData()
     }
+}
+
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -109,4 +115,4 @@ class SearcherTableViewController: UITableViewController,UISearchBarDelegate {
     }
     */
 
-}
+
